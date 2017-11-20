258a8f311876e6c1c84c3588ea1234f45fccc220
Andrew Crosby <awcrosby@gmail.com>
2017-11-20 13:55:28 -0500

    completed routing section

---
+++ s16routing/src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <ul class="nav nav-pills">
+        <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+        <router-link to="/user/" tag="li" active-class="active"><a>User</a></router-link>
+
+        <!-- <li role="presentation"><router-link to="/">Home</router-link></li>
+        <li role="presentation"><router-link to="/user">User</router-link></li> -->
+    </ul>
+</template>
---
+++ s16routing/src/routes.js
@@ -0,0 +1,44 @@
+import Home from './components/Home.vue';
+import Header from './components/Header.vue';
+
+//for lazy loading into webpack, only download these build.js if needed
+const User = resolve => {
+    require.ensure(['./components/user/User.vue'], () => {
+        resolve(require('./components/user/User.vue'));
+    });
+};
+const UserStart = resolve => {
+    require.ensure(['./components/user/UserStart.vue'], () => {
+        resolve(require('./components/user/UserStart.vue'));
+    });
+};
+const UserDetail = resolve => {
+    require.ensure(['./components/user/UserDetail.vue'], () => {
+        resolve(require('./components/user/UserDetail.vue'));
+    });
+};
+const UserEdit = resolve => {
+    require.ensure(['./components/user/UserEdit.vue'], () => {
+        resolve(require('./components/user/UserEdit.vue'));
+    });
+};
+
+export const routes = [
+    { path: '', name: 'home', components: {
+        default: Home,
+        'header-top': Header
+    } },
+    { path: '/user', components: {
+        default: User,
+        'header-bottom': Header
+    }, children: [
+        { path: '', component: UserStart },
+        { path: ':id', component: UserDetail, beforeEnter: (to, from, next) => {
+            console.log('inside route setup');
+            next();
+        } },
+        { path: ':id/edit', component: UserEdit, name: 'userEdit' }
+    ] },
+    { path: '/redirect-me', redirect: '/user' },
+    { path: '*', redirect: '/'}
+];
