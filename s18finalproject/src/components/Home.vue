8de811b64b7e8bac8ebf8ade0cee446cd9c1a42f
Andrew Crosby <awcrosby@gmail.com>
2017-11-29 17:00:58 -0500

    s18 module

---
+++ s18finalproject/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Trade or View your portfolio</h1>
+        <h6>You may save or load your data</h6>
+        <h6>Click on 'End Day' to begin a new day</h6>
+        <hr>
+        <p>Your funds: {{ funds }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            funds() {
+                return this.$store.getters.funds;
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
